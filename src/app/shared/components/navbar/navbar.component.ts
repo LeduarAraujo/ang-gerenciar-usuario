0ad9e3caa386632fb586f75d9ef7379625b8faf7
leduararaujo <leduar.araujo@hotmail.com>
2023-08-30 17:44:57 -0300

    #6 Ajuste da sessão de usuario para pegar o token

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -11,14 +11,34 @@ import { AtomicoService } from 'src/app/core/services/atomico.service';
 export class NavbarComponent {
 
   @Input() usuario: Usuario = new Usuario;
+  @Input() token: { token_jwt: string };
 
   constructor(
     private atomicoService: AtomicoService,
     private router: Router
-  ) {
+  ) { this.validarSessao() }
 
+  validarSessao() {
+    debugger
+    this.token = (this.router.getCurrentNavigation()?.extras.state as {
+      token_jwt: string
+    });
 
-
+    if (this.token) {
+      this.atomicoService.getUsuarioLogado(this.token.token_jwt).subscribe(
+        (retorno: Usuario) => {
+          this.usuario = retorno;
+        },
+        err => this.router.navigate(['/login'])
+      );
+    } else { this.router.navigate(['/login']) }
   }
 
+  direcionarRota(rota: string) {
+    this.router.navigate([rota], {state: this.token});
+  }
+
+  deslogar() {
+    this.router.navigate(['/login']);
+  }
 }
